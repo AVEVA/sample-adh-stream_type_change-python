a166de0e3397f6a6fd5a5afd1120b32e7ec3018f
Kelsey Duffy <kduffy@osisoft.com>
2022-01-10 14:56:29 -0500

    PR comments - console and log file logging parity, reject empty conversions

--- program.py
+++ program.py
@@ -24,6 +24,10 @@ def affirmative_response(response):
     affirmative_responses = ['y', 'yes']
     return response.lower() in affirmative_responses
 
+def output(level, message):
+    logging.log(level, message)
+    print(message)
+
 def generate_adapter_upgrade_mappings(adapter_type, ocs_client, namespace_id, test):
     """This function takes in an adapter type (such as 'OpcUa'), generates the necessary stream views,
     and returns a mapping table for the existing type to the stream view that maps it to the new type.
@@ -35,12 +39,12 @@ def generate_adapter_upgrade_mappings(adapter_type, ocs_client, namespace_id, te
 
     # If a known imcompatible adapter is attempted, stop the script and log an error
     if adapter_type.lower() in incompatible_adapter_types:
-        logging.error(f'The adapter type of {adapter_type} is known to be incompatible with this script. Please perform the type changes manually or contact support for assistance.')
+        output(logging.ERROR, f'The adapter type of {adapter_type} is known to be incompatible with this script. Please perform the type changes manually or contact support for assistance.')
         raise Exception(f'Incompatible adapter type of {adapter_type} detected')
 
     # If the specified adapter type is not known to work or known to fail, warn and continue. This could be a typo in the settings file, or a new untested adapter type
     if adapter_type.lower() not in tested_adapter_types:
-        logging.warning(f'Encountered untested adapter type of {adapter_type}. The konwn tested adapter types are {", ".join(tested_adapter_types)}. If this was unintended, please rerun the script with the correct adapter type.')
+        output(logging.WARNING, f'Encountered untested adapter type of {adapter_type}. The konwn tested adapter types are {", ".join(tested_adapter_types)}. If this was unintended, please rerun the script with the correct adapter type.')
 
 
     mapping = {}
@@ -49,28 +53,33 @@ def generate_adapter_upgrade_mappings(adapter_type, ocs_client, namespace_id, te
     type_search_query = f'TimeIndexed.* AND *.{adapter_type}Quality'
     new_types = ocs_client.Types.getTypes(namespace_id, query=type_search_query)
 
+    if len(new_types) == 0:
+        output(logging.ERROR, 'No PI Adapter v1.2 types were detected on the namespace for this adapter type. Be sure to upgrade the adapter before running this script. Quitting...')
+        raise Exception(f'No PI Adapter v1.2 types were detected on this namespace.')
+
+
     if test:
         # If this script is being E2E tested, presume the user input to be y
         response = 'y'
-        logging.debug(f'Automated test will begin stream view creation without prompting the user, assuming a response of {response}.')
+        output(logging.DEBUG, f'Automated test will begin stream view creation without prompting the user, assuming a response of {response}.')
         
 
     else: 
         # Before creating the stream views, user confirmation is requested
-        logging.info(f'Found {len(new_types)} types that are potentially going to be have stream views created to map existing types to them.')
-        print(f'Found {len(new_types)} types that are potentially going to be have stream views created to map existing types to them.')
+        output(logging.INFO, f'Found {len(new_types)} types that are potentially going to be have stream views created to map existing types to them.')
         logging.debug(f'Prompting user whether they would like to see the list of new type IDs.')
         response = input('Would you like to see their IDs? (y/n): ')
+        logging.debug(f'Response: {response}')
         print()
 
         if affirmative_response(response):
             for new_type in new_types:
-                logging.info(new_type.Id)
-                print(new_type.Id)
+                output(logging.INFO, new_type.Id)
 
         print()
         logging.debug(f'Prompting user whether they would like to continue with the stream view creations.')
         response = input('Would you like to create the stream views? (y/n): ')
+        logging.debug(f'Response: {response}')
         print()
     
     if affirmative_response(response):
@@ -83,8 +92,8 @@ def generate_adapter_upgrade_mappings(adapter_type, ocs_client, namespace_id, te
             # The 'simple' types (eg. TimeIndexed.Int32.OpcUaQuality) are three pieces: 0 = 'TimeIndexed'; 1 = <data type>; 2 = '<adapter_type>Quality
             # Others, such as 'enum' types are more pieces and cannot be migrated with this script
             if len(type_name_parts) > 3:
-                logging.warning(f'Non-simple type detected. No streams will be automatically migrated to {new_type}...')
-                logging.warning(f'...If possible, the existing steams will be converted to their corresponding integer quality type.')
+                output(logging.WARNING, f'Non-simple type detected. No streams will be automatically migrated to {new_type}...')
+                output(logging.WARNING, f'...If possible, the existing steams will be converted to their corresponding integer quality type.')
                 continue
 
             # The version 1.1 SDS Type ID is the first two parts joined back together (eg. TimeIndexed.Int32.OpcUaQuality -> TimeIndexed.Int32)
@@ -98,20 +107,22 @@ def generate_adapter_upgrade_mappings(adapter_type, ocs_client, namespace_id, te
             this_stream_view_id = f'{adapter_type}_{data_type}_quality'
             this_stream_view = SdsStreamView(id=this_stream_view_id, source_type_id=existing_type_id, target_type_id=new_type.Id)
 
-            logging.info(f'Creating streamview with id {this_stream_view_id} mapping {existing_type_id} to {new_type.Id}...')
+            output(logging.INFO, f'Creating stream view with id {this_stream_view_id} mapping {existing_type_id} to {new_type.Id}...')
             try:
                 this_stream_view = ocs_client.StreamViews.getOrCreateStreamView(namespace_id, this_stream_view)
             except Exception as error:
                 # Log the error, but don't raise the exception. This failure is only a problem if it causes a stream to fail to convert, which will be caught later as a separate exception
-                logging.error(f'Encountered error while creating stream view: {error}')
+                output(logging.ERROR, f'Encountered error while creating stream view with id {this_stream_view_id}: {error}')
+                output(logging.ERROR, f'This script will continue but may fail later when attempting to convert streams from {existing_type_id} to {new_type.Id}.')
+                
                 
             # add the streamview id to the mappings list under the key of the existing type id
             mapping[existing_type_id] = this_stream_view.Id
         
-        logging.info('Done creating stream views.')
+        output(logging.INFO, 'Done creating stream views.')
 
     else:
-        logging.info('Returning blank mapping table')
+        output(logging.INFO, 'Returning blank mapping table')
         
     return mapping
 
@@ -123,7 +134,7 @@ def main(test=False):
         appsettings = get_appsettings()
 
         # Read configuration from appsettings.json and create the OCS client object
-        logging.debug('Authenticating to OCS...')
+        output(logging.DEBUG, 'Authenticating to OCS...')
         ocs_client = OCSClient(appsettings.get('ApiVersion'),
                                 appsettings.get('TenantId'),
                                 appsettings.get('Resource'),
@@ -150,34 +161,40 @@ def main(test=False):
         # Get streams in the namespace
         streams = ocs_client.Streams.getStreams(namespace_id, query=stream_search_query)
 
+        if len(streams) == 0:
+            message = f'No stream found on namespace {namespace_id} that match the stream search pattern of {stream_search_query}'
+            output(logging.ERROR, message)
+            output(logging.ERROR, 'Quitting script...')
+            raise Exception(message)
+
         if test:
             # If this script is being E2E tested, presume the user input to be y
             response = 'y'
-            logging.debug(f'Automated test will begin type conversion without prompting the user, assuming a response of {response}.')
+            output(logging.DEBUG, f'Automated test will begin type conversion without prompting the user, assuming a response of {response}.')
 
         else: 
             # Before changing the streams, user confirmation is requested
             print()
-            logging.info(f'Found {len(streams)} streams that are potentially going to be converted using stream view.')
-            print(f'Found {len(streams)} streams that are potentially going to be converted using stream view.')
+            output(logging.INFO, f'Found {len(streams)} streams that are potentially going to be converted using stream view.')
             print()
             logging.debug(f'Prompting user whether they would like to see the list of stream IDs.')
             response = input('Would you like to see their IDs? (y/n): ')
+            logging.debug(f'Response: {response}')
             print()
 
             if affirmative_response(response):
                 for stream in streams:
-                    logging.info(f'ID: {stream.Id} Name: {stream.Name}')
-                    print(f'ID: {stream.Id} Name: {stream.Name}')
+                    output(logging.INFO, f'ID: {stream.Id} Name: {stream.Name}')
 
             print()
             logging.debug(f'Prompting user whether they would like to continue with the stream type edits.')
             response = input('Would you like to continue with the type conversions? (y/n): ')
+            logging.debug(f'Response: {response}')
             print()
 
         if affirmative_response(response):
 
-            logging.info('Processing streams...')
+            output(logging.INFO, 'Processing streams...')
             
             # Keep track of the streams processed and skipped
             converted_streams = 0
@@ -188,34 +205,34 @@ def main(test=False):
 
                 # Look for the stream's existing type in the mappings table. If it's there, apply the stream view
                 if stream.TypeId in type_to_stream_view_mappings:
-                    logging.info(f'Changing type of {stream.Id} away from {stream.TypeId} using steamview id {type_to_stream_view_mappings[stream.TypeId]}...')
+                    output(logging.INFO, f'Changing type of {stream.Id} away from {stream.TypeId} using steamview id {type_to_stream_view_mappings[stream.TypeId]}...')
                     try:
                         ocs_client.Streams.updateStreamType(namespace_id, stream_id=stream.Id, stream_view_id=type_to_stream_view_mappings[stream.TypeId])
                         converted_streams += 1
                     except Exception as error:
-                        logging.error(f'Encountered error while converting stream: {error}')
+                        output(logging.ERROR, f'Encountered error while converting stream: {error}')
                         exception = error
                         failed_streams += 1
 
                 # If it's not, skip it and notify the user why it wasn't processed
                 else:
-                    logging.warning(f'Skipped {stream.Id} because it has a type of {stream.TypeId}, which is not in the mappings table. It will need to be migrated separately.')
+                    output(logging.WARNING, f'Skipped {stream.Id} because it has a type of {stream.TypeId}, which is not in the mappings table. It will need to be migrated separately.')
                     skipped_streams += 1
 
             # Log the final tallies of each counter  
-            logging.info(f'Operation completed. Successfully converted {converted_streams} streams.')
+            output(logging.INFO, f'Operation completed. Successfully converted {converted_streams} streams.')
             
             # If a stream failed or was skipped, log it was a warning. Otherwise, log it as info
             if failed_streams > 0 or skipped_streams > 0:
-                logging.warning(f'Operation incomplete. Failed to convert {failed_streams} streams and skipped {skipped_streams} streams.')
+                output(logging.WARNING, f'Operation incomplete. Failed to convert {failed_streams} streams and skipped {skipped_streams} streams.')
             else:
-                logging.info(f'No streams failed to convert or were skipped.')
+                output(logging.INFO, f'No streams failed to convert or were skipped.')
 
         else:
-            logging.info('Exiting. No transformation will be attempted.')
+            output(logging.INFO, 'Exiting. No transformation will be attempted.')
 
     except Exception as error:
-        logging.error(f'Encountered Error: {error}')
+        output(logging.ERROR, f'Encountered Error: {error}')
         traceback.print_exc()
         exception = error
 
@@ -237,7 +254,7 @@ if __name__ == '__main__':
 
     # Set up the logger
     logging.basicConfig(filename=log_file_name, encoding='utf-8', level=level, datefmt='%Y-%m-%d %H:%M:%S', format= '%(asctime)s %(module)16s,line: %(lineno)4d %(levelname)8s | %(message)s')
-    logging.info('Starting Stream Type change sample')
+    output(logging.INFO, 'Starting Stream Type change sample')
 
     try:
         # Run the sample
@@ -246,4 +263,4 @@ if __name__ == '__main__':
         # No except block is necessary as exceptions will be logged by the sample itself
     finally:
         # Write a message that the logger is done.
-        logging.info('Stream Type change sample completed')
+        output(logging.INFO, 'Stream Type change sample completed')
